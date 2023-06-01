32698a03db776b41cd1a6a9444a375607703a863
Dagets <inard.frederic@gmail.com>
2023-06-01 10:53:44 +0200

    exercice 3 finish

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,7 +1,32 @@
-function PokemonCard() {
-  return <figure>
-            <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="image Bulbi" />
-            <figcaption> bulbasaur</figcaption>
-         </figure>;
+const pokemonList = [
+  {
+    name: "bulbasaur",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+  },
+  {
+    name: "mew",
+  },
+];
+
+const PokemonCard = () => {
+  
+  let pokemon = pokemonList[1]
+  // console.log(pokemon.imgSrc != undefined ? "on a l'image" : "???")
+  // if (pokemon.imgSrc != undefined){
+  //   <img src={pokemon.imgSrc} alt={pokemon.name}/>
+
+  // } else {
+  //   <p>???</p>
+  // }
+
+  return (
+    <figure>
+      {pokemon.imgSrc != undefined ? <img src={pokemon.imgSrc} alt={pokemon.name}/> : <p>???</p>}
+      <figcaption> {pokemon.name}</figcaption>
+    </figure>
+  )
+   
 }
 export default PokemonCard;
+
