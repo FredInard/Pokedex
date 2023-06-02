f6cdacc5463234440257ed5d7c12204623e35210
Dagets <inard.frederic@gmail.com>
2023-06-02 14:15:46 +0200

    commit pour sortir

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,23 +1,34 @@
-import PropTypes from "prop-types";
+// import PropTypes from "prop-types";
 
-const PokemonCard = ({pokemon}) => {
+// const PokemonCard = ({pokemon}) => {
   
-console.log('test'+ {pokemon})
+// console.log('test'+ {pokemon})
 
-  return (
-    <figure>
-      {pokemon.imgSrc != undefined ? <img src={pokemon.imgSrc} alt={pokemon.name}/> : <p>???</p>}
-      <figcaption> {pokemon.name}</figcaption>
-    </figure>
-  )
+//   return (
+//     <figure>
+//       {pokemon.imgSrc != undefined ? <img src={pokemon.imgSrc} alt={pokemon.name}/> : <p>???</p>}
+//       <figcaption> {pokemon.name}</figcaption>
+//     </figure>
+//   )
+// }
+
+// PokemonCard.PropTypes = {
+//   pokemonList: PropTypes.shape({
+//     name: PropTypes.string.isRequired,
+//     imgSrc:PropTypes.string,
+//   }).isRequired
+// }
+
+// export default PokemonCard;
+
+
+function PokemonCard() {
+  return(
+
+ <figure> <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="" /> </figure>
+  );
+
 }
 
-PokemonCard.PropTypes = {
-  pokemonList: PropTypes.shape({
-    name: PropTypes.string.isRequired,
-    imgSrc:PropTypes.string,
-  }).isRequired
-}
 
 export default PokemonCard;
-
